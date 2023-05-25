1ffe6db75eae03dd0be1a40e5a6d5d9327fb4360
Cristian camilo <cristianmilo07@gmail.com>
2023-05-25 18:15:14 -0500

    terminado

--- index.js
+++ index.js
@@ -1,51 +1,73 @@
-import { inquirerMenu, pausa, leerInput } from './helpers/inquirer.js';
-import { Busquedas }  from './models/busqueda.js';
+require('dotenv').config()
+
+
+const { leerInput, inquirerMenu, pausa, listarLugares } = require('./helpers/inquirer');
+const Busquedas = require('./models/busquedas');
 
 const main = async() => {
 
-    const busqueda = new Busquedas();
+    const busquedas = new Busquedas();
+    let opt;
 
-    let opt = '';
+    do{
 
-    //await pausa();
-    do {
-        //imprimir el menú
         opt = await inquirerMenu();
-            switch (opt) {
-                case '1':
+        
+        switch( opt ) {
+
+            case 1:
                 // Mostrar mensaje
                 const termino = await leerInput('Ciudad: ');
-
+                
                 // Buscar los lugares
+                const lugares = await busquedas.ciudad( termino );
                 
                 // Seleccionar el lugar
+                const id = await listarLugares(lugares);
+                if ( id === '0' ) continue;
+
+                const lugarSel = lugares.find( l => l.id === id );
 
                 // Guardar en DB
+                busquedas.agregarHistorial( lugarSel.nombre );
 
                 // Clima
+                const clima = await busquedas.climaLugar( lugarSel.lat, lugarSel.lng );
 
                 // Mostrar resultados
                 console.clear();
                 console.log('\nInformación de la ciudad\n'.green);
-                console.log('Ciudad:', );
-                console.log('Lat:',  );
-                console.log('Lng:',  );
-                console.log('Temperatura:', );
-                console.log('Mínima:', );
-                console.log('Máxima:', );
-                console.log('Como está el clima:', );
-                    
-                break;
-                
-                case '2':
+                console.log('Ciudad:', lugarSel.nombre.green );
+                console.log('Lat:', lugarSel.lat );
+                console.log('Lng:', lugarSel.lng );
+                console.log('Temperatura:', clima.temp );
+                console.log('Mínima:', clima.min );
+                console.log('Máxima:', clima.max );
+                console.log('Como está el clima:',  clima.desc.green );
 
-                break;
+            break;
 
-            }
-            await pausa();
 
-    } while (opt !== '0') ;
+            case 2:
+                 busquedas.historialCapitalizado.forEach( (lugar, i) =>  {
+                     const idx = `${ i + 1 }.`.green;
+                     console.log( `${ idx } ${ lugar } ` );
+                 })
+
+            break;
+
+        }
+
+
+
+        if ( opt !== 0 ) await pausa();
+
+    } while ( opt !== 0 )
+
+
+
 }
+
 
 
 main();
